af0dadf16b4fa6998d9e05a814055a2bdd313d46
Robin Olofsson <robin.olofsson@storytel.com>
2023-11-23 14:05:14 +0100

    Update main.js

--- .github/actions/consent/main.js
+++ .github/actions/consent/main.js
@@ -11,19 +11,25 @@ async function run() {
 
   const octokit = github.getOctokit(token)
   console.log(token, octokit, context)
-
-  const { data: pullRequest } = await octokit.rest.pulls.listFiles({
+  
+  const { data: pullRequest } = await octokit.rest.pulls.get({
     owner: context.repo.owner,
     repo: context.repo.repo,
     pull_number: context.payload.pull_request.number,
   });
 
-  pullRequest.forEach(file => {
+  const { data: files } = await octokit.rest.pulls.listFiles({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    pull_number: context.payload.pull_request.number,
+  });
+
+  files.forEach(file => {
     console.log("source", getFileContent(context.repo.owner, context.repo.repo, "main"))
-    console.log("new", getFileContent(context.repo.owner, context.repo.repo, file.filename, context.payload.pull_request.ref));
+    console.log("new", getFileContent(context.repo.owner, context.repo.repo, file.filename, pullRequest.head.ref));
   })
 
-  console.log(pullRequest)
+  console.log(pullRequest, files)
 }
 
 async function getFileContent(owner, repo, path, ref) {
