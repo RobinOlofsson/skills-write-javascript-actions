8b704e226b4a9cf102e0ccfd25dae75b1c0ea2a5
Robin Olofsson <robin.olofsson@ingrid.com>
2024-10-18 11:15:43 +0200

    fdsa

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.2"
+    }
+  }
+}
+
 resource "local_file" "test" {
   filename = "README.md"
   content  = "Hello world!"
